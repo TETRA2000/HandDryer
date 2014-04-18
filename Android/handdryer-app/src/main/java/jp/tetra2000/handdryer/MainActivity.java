01ad5ae418bb9681fe4442a341001d2e9f9b8bf3
TETRA2000 <sagittariusm25@gmail.com>
2014-04-18 21:42:23 +0900

    remove menu

--- Android/handdryer-app/src/main/java/jp/tetra2000/handdryer/MainActivity.java
+++ Android/handdryer-app/src/main/java/jp/tetra2000/handdryer/MainActivity.java
@@ -10,7 +10,6 @@ import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.Toast;
 
@@ -74,17 +73,17 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     private void onDryingStateChanged(boolean isDrying) {
         if(isDrying)
