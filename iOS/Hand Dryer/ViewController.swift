d8a77698875fcc1868495ef7a07ff4b04d3f2b31
TETRA2000 <sagittariusm25@gmail.com>
2015-12-13 22:07:13 +0900

    iosversion

--- iOS/Hand Dryer/ViewController.swift
+++ iOS/Hand Dryer/ViewController.swift
@@ -7,19 +7,93 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    private var audioSession: AVAudioSession?
+    private var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // keep screen awake
+        UIApplication.sharedApplication().idleTimerDisabled = true
+        
+        configureAudioSession()
+        
+        setupSound()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        startMonitorProximity()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        stopMonitorProximity()
+        
+        super.viewWillDisappear(animated)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    private func startMonitorProximity() {
+        UIDevice.currentDevice().proximityMonitoringEnabled = true;
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("proximityChanged"), name: "UIDeviceProximityStateDidChangeNotification", object: nil);
+    }
+    
+    private func stopMonitorProximity() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+        
+        UIDevice.currentDevice().proximityMonitoringEnabled = false;
+    }
+    
+    func proximityChanged() {
+        if UIDevice.currentDevice().proximityState {
+            self.player!.play()
+        } else {
+            self.player!.stop()
+        }
+    }
+    
+    func configureAudioSession() {
+        audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession!.setCategory(AVAudioSessionCategoryPlayback)
+        } catch is NSError {
+            // TODO: DO SOMETHINGT
+            abort()
+        }
+    }
+    
+    func setupSound() {
+        let path = NSBundle.mainBundle().pathForResource("dryer", ofType: "m4a")
+        let url = NSURL(fileURLWithPath: path!)
+        do {
+            try self.player = AVAudioPlayer(contentsOfURL: url)
 
-
+            
+        }catch is NSError {
+            // FIXME: Do something
+            abort()
+        }
+        
+        self.player!.delegate = self
+        self.player!.numberOfLoops = -1
+        self.player!.prepareToPlay()
+    }
+    
+    // AVAudioPlayerDelegate:
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        // TODO:
+    }
+    
 }
 
