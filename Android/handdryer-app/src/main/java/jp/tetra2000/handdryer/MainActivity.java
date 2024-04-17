6f889a238df0098ecbaff1ca0e3b08b029e6d31f
TETRA2000 <sagittariusm25@gmail.com>
2024-04-17 23:23:03 +0900

    Upgrade to latest Android tool chains

--- Android/handdryer-app/src/main/java/jp/tetra2000/handdryer/MainActivity.java
+++ Android/handdryer-app/src/main/java/jp/tetra2000/handdryer/MainActivity.java
@@ -8,13 +8,13 @@ import android.hardware.SensorManager;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
     private SensorManager mManager;
     private Sensor mProximitySensor;
     private float mProximityRange;
